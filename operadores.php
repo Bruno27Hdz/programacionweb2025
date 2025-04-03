147aa285ab61a09a03ecf8197a0e164f0b41f96e
Bruno27Hdz <Bru2007hdz@gmail.com>
2025-04-03 14:17:34 -0300

    Operadores de Asignacion

---
+++ operadores.php
@@ -0,0 +1,24 @@
+<?php
+
+#OPERADORES DE ASIGNACION
+
+#ASIGNACION "=": Guarda un valor a una variable
+    $emisora = "oceano";
+
+#SUMA Y ASIGNACION "+=": Agrega un valor x a una variabla ya guardada
+    $edad = 1; $edad +=1; echo $edad;
+
+#RESTA Y ASIGNACION "-=": Resta un valor x a una variable ya guardada
+    $edad = 1; $edad +=10; echo $edad;
+
+#MULTIPLICACION Y ASIGNACION "*=": multiplica un valor x a una variable ya guardada 
+    $edad = 1; $edad *=100; echo $edad;
+
+#DIVISION Y ASIGNACION "/="
+#MODULO Y ASIGNACION "%="
+#CONCATENACION Y ASIGNACION ".="
+
+
+
+
+?>
