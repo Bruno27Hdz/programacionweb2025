75f976bbdab43a8e6f389573d212e401a63de2ed
Bruno27Hdz <Bru2007hdz@gmail.com>
2025-04-03 13:59:21 -0300

    Ejemplo de concatenacion con comillas

---
+++ comillas.php
@@ -0,0 +1,15 @@
+<?php
+        $emisora = 'Radio Disney';
+        echo "Me gusta escuchar $emisora";
+
+        $emisora = 'Radio Disney';
+        echo 'Me gusta escuchar $emisora';
+
+/*
+CONCATENAR significa UNIR O JUNTAS dos o más cadenas de texto (o elementos) en una sola.
+ la concatenación se refiere a la operación de combinar varias cadenas de caracteres en una sola secuencia. Por ejemplo, 
+ si tienes las cadenas "Hola" y "Mundo", al concatenarlas obtienes "Hola Mundo".
+*/
+
+
+?>
