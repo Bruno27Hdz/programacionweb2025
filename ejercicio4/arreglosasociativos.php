22145fe2ac96ab3a4f3289bc4a42274a3073269c
bruno27hdz <bru2007hdz@gmail.com>
2025-08-05 17:31:00 -0300

    e

---
+++ ejercicio4/arreglosasociativos.php
@@ -0,0 +1,18 @@
+<?php 
+    # ACTIVIDAD 1.. ARREGLO ASOCIATIVO
+
+        $videojuego = [ 'nombre' => 'The Legend Of Zelda',
+                        'consola' => 'NES',
+                        'lanzamiento' => 1986,
+                        'precio' => 50];
+
+    #ACTIVIDAD 2
+        foreach ($videojuego as $key => $value)
+                echo $value;  
+
+
+
+
+
+
+?>
