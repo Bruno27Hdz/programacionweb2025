46c8bc1af95227be08162cf14afd0796570374cd
Bruno27Hdz <bru2007hdz@gmail.com>
2025-08-07 15:33:43 -0300

    practico arreglos asociativos

--- ejercicio4/arreglosasociativos.php
+++ ejercicio4/arreglosasociativos.php
@@ -11,10 +11,32 @@
                 echo "$clave $valor<br>" ;
 
     #ACTIVIDAD3 
-    $videojuego = ['stock' => 10];
-    if ($videojuego['stock'] > 1) {
-        echo "AUN QUEDAN ". $videojuego['stock']. " !!!";;
-    }
+        $videojuego = ['stock' => 10];
+        if ($videojuego['stock'] > 1) {
+            echo "AUN QUEDAN ". $videojuego['stock']. " !!!";;
+        }
 
+    #ACTIVIDAD 4 
+    
+        $inventario = ["Chrono Trigger" => 
+                            ['consola' => 'SNES',
+                            'lanzamiento' => 1995,
+                            'precio' => 80.00,
+                            'stock' => 5 ],
+                        "Super Mario Bros" => ['consola' => 'NES',
+                            'lanzamiento' => 1988,
+                            'precio' => 45.00,
+                            'stock' => 15 ],
+                        ];
+        $inventario["The Legend Of Zelda"]=['consola' => 'NES',
+                            'lanzamiento' => 1986,
+                            'precio' => 50];
+            echo "VIDEOJUEGOS EN INVENTARIO: ".$inventario;
 
-?>
+    #ACTIVIDAD 5
+            foreach ($videojuego as $clave => $valor) {
+                echo "Titulo: ". $inventario['Chrono Trigger'];
+            }
+        
+    
+    ?>
